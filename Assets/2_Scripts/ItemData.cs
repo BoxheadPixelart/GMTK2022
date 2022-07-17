a61219adb8522174be2030dcdb415800360ab0fc
Storm Hughes <storm.hughes8@gmail.com>
2022-07-16 21:20:18 -0500

    REWORKING ARMS ONE SEC LOL ALSO FLATKIT

--- Assets/2_Scripts/ItemData.cs
+++ Assets/2_Scripts/ItemData.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class ItemData : ScriptableObject
 {
+    public ItemId id;
+    public bool canBeGrabbed;
     [SerializeField] public string itemName; 
     [SerializeField] public Mesh itemMesh;
     [SerializeField] public float itemMass;
@@ -15,4 +17,10 @@ public class ItemData : ScriptableObject
     [SerializeField] public float itemRadius; 
     [SerializeField] public float itemHeight;
 
+    public enum ItemId
+    {
+        Nul = -1,
+        Tube = 0,
+        Max
+    }
 } 
