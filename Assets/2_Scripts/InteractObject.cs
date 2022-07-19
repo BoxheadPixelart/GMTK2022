3d2e064e8dd8e971a5285c309d120eb953b9fe94
Storm Hughes <storm.hughes8@gmail.com>
2022-07-18 23:00:27 -0500

    whoops

--- Assets/2_Scripts/InteractObject.cs
+++ Assets/2_Scripts/InteractObject.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class InteractObject : MonoBehaviour
 {
+    public bool isIdle;
+    public Animator anim;
+    public float interactionTime;
+
+    private IEnumerator taskRoutine;
+    private Coroutine taskCoroutine; 
     // Start is called before the first frame update
-    void Start()
+    public void StartInteraction()
     {
+        taskRoutine = TaskRoutine(interactionTime);
+        taskCoroutine = StartCoroutine(taskRoutine);
         
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CancelTask()
     {
-        
+        if (taskRoutine.MoveNext())
+        {
+            StopCoroutine(taskCoroutine);
+        }
+    }
+
+    IEnumerator TaskRoutine(float time)
+    {
+        print("Task Started");
+
+        print("Task Ended");
+        yield return null; 
     }
 }
